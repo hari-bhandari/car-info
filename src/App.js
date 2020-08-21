260c3f48ded9fca875cbf0a70fbe7e94eaf8d40e
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-08-21 15:18:38 +0100

    added about section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import CarAnimation from "./Body/CarAnimation";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import AboutCar from "./Body/AboutCar";
 function App() {
   return (
       <Router>
           <div>
               <Switch>
                   <Route exact path={'/'} component={CarAnimation}/>
-                  <Route exact path={'/:id'} component={CarAnimation}/>
+                  <Route exact path={'/:id'} component={AboutCar}/>
 
               </Switch>
           </div>
---
+++ src/Body/AboutCar.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import './AboutMe.css'
+const AboutCar = () => {
+    return (
+        <div className="section-fluid-main">
+            <div className="section">
+                <div className="info-wrap mob-margin">
+                    <p className="title-up">Modern chair</p>
+                    <h2>Sella Gastro</h2>
+                    <h4>$174 <span>$237</span></h4>
+                    <div className="section-fluid">
+                        <input className="desc-btn" type="radio" id="desc-1" name="desc-btn" checked/>
+                        <label htmlFor="desc-1">Description</label>
+                        <input className="desc-btn" type="radio" id="desc-2" name="desc-btn"/>
+                        <label htmlFor="desc-2">Details</label>
+                        <div className="section-fluid desc-sec accor-1">
+                            <p>The chair construction is made of ash tree. Upholstery and wood color at customer's request.</p>
+                        </div>
+                        <div className="section-fluid desc-sec accor-2">
+                            <div className="section-inline">
+                                <p><span>76</span>cm<br/>Length</p>
+                            </div>
+                            <div className="section-inline">
+                                <p><span>68</span>cm<br/>Width</p>
+                            </div>
+                            <div className="section-inline">
+                                <p><span>87</span>cm<br/>Height</p>
+                            </div>
+                            <div className="section-inline">
+                                <p><span>10</span>kg<br/>Weight</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <input className="color-btn for-color-1" type="radio" id="color-1" name="color-btn" checked/>
+
+                <div className="img-wrap chair-1"></div>
+
+
+                <a href="https://front.codes/" className="logo" target="_blank">
+                    <img src="https://assets.codepen.io/1462889/fcy.png" alt=""/>
+                </a>
+            </div>
+        </div>
+    );
+};
+
+export default AboutCar;
