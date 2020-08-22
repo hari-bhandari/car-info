f8178148a527e3efda1782e97446a539f7a06a4d
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-08-22 14:05:35 +0100

    integrated context api to searchCar

--- src/Body/SearchBar.js
+++ src/Body/SearchBar.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './SearchBar.css'
 const SearchBar = () => {
+    const[search,setSearch]=useState()
+    const onSubmit=(e)=>{
+        e.preventDefault()
+    }
     return (
-        <form onSubmit="event.preventDefault();" role="search">
+        <form onSubmit= role="search">
             <label htmlFor="search">Search for stuff</label>
-            <input id="search" type="search" placeholder="Search..." autoFocus required/>
+            <input id="search" type="search" placeholder="Search..." onChange={(e)=>{
+                setSearch(e.target.value)
+            }} autoFocus required/>
             <button type="submit">Go</button>
         </form>
     );
