418312533dc94b92714e4339eb283d5e8592f2fc
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-08-23 22:13:13 +0100

    Updates

---
+++ precache-manifest.1333698b6e0859eba6fbb000c0d5d719.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3a5d71eadefb4e45791bba3527531fd",
+    "url": "/car-info/index.html"
+  },
+  {
+    "revision": "bae40a530afd17ac9edb",
+    "url": "/car-info/static/css/main.a4d37319.chunk.css"
+  },
+  {
+    "revision": "3733402fd281a2b47f81",
+    "url": "/car-info/static/js/2.fc7f61e5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/car-info/static/js/2.fc7f61e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bae40a530afd17ac9edb",
+    "url": "/car-info/static/js/main.6da0ab32.chunk.js"
+  },
+  {
+    "revision": "ab3e71995c71a8bede48",
+    "url": "/car-info/static/js/runtime-main.4d3c8b97.js"
+  },
+  {
+    "revision": "421293eff9250c57597dce7352d38448",
+    "url": "/car-info/static/media/car-b.421293ef.png"
+  },
+  {
+    "revision": "71ecff0cb783117dbb8e0512b68f79e1",
+    "url": "/car-info/static/media/car.71ecff0c.png"
+  },
+  {
+    "revision": "c6733be77a936297512bcd2a2a2371a3",
+    "url": "/car-info/static/media/city.c6733be7.png"
+  },
+  {
+    "revision": "ab54778e83b7e64c1ff949077c6c5ce0",
+    "url": "/car-info/static/media/logo1.ab54778e.png"
+  },
+  {
+    "revision": "6efb0aeacb5bba5f4308dd20e420855d",
+    "url": "/car-info/static/media/road.6efb0aea.webp"
+  },
+  {
+    "revision": "350ae0c733a2a6410c672417af63a57f",
+    "url": "/car-info/static/media/sky.350ae0c7.webp"
+  }
+]);
